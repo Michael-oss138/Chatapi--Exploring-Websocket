c38292820d2545be01459cbf1db73d9a8f4237f7
Michael-oss138 <nwaizumichael0@gmail.com>
2025-09-30 00:42:02 +0100

    Rounded up the models.

--- models.py
+++ models.py
@@ -7,8 +7,27 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    full_name = db.Column(db.String(50), nullable=True)
+    password_hash = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
+    def set_password(self, raw_password: str):
+        self.password_hash = generate.password_hash(raw_password)
+
+
+    def check_password(self, raw_password: str) -> bool:
+        return check_password_hash(self.password_hash, raw_password)
+
+
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "full_name": self.full_name,
+            "created_at": self.created_at.isoformar()
+        }
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key= True)
